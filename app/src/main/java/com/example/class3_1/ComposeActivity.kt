375c7be7f11aeda8acdb683fdbb22133675adb94
maksym_oboznyi <obozniy.maxim@ukr.net>
2022-08-09 19:30:41 +0200

    row at the bottom

--- app/src/main/java/com/example/class3_1/ComposeActivity.kt
+++ app/src/main/java/com/example/class3_1/ComposeActivity.kt
@@ -3,16 +3,16 @@ package com.example.class3_1
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -23,7 +23,9 @@ class ComposeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Column(
-                modifier = Modifier.fillMaxSize().background(Color.Black),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Black),
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally,
 
@@ -37,6 +39,23 @@ class ComposeActivity : AppCompatActivity() {
                     ),
                     modifier = Modifier.width(300.dp)
                 )
+                Row(
+                    horizontalArrangement = Arrangement.Center,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.android),
+                        contentDescription = "TODO",
+                        modifier = Modifier.size(150.dp, 150.dp)
+
+                    )
+                    Image(
+                        painter = painterResource(id = R.drawable.kotlin),
+                        contentDescription = "TODO",
+                        modifier = Modifier.size(140.dp, 140.dp)
+                    )
+
+                }
             }
         }
     }
